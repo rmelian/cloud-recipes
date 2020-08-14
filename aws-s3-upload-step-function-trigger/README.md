6e24a42469875356bd2f7a2f360cd0865fbce1a9
Raisel Melian <raisel.melian@viacom.com>
2020-08-14 15:54:41 -0500

    update readme and lamnda function

--- aws-s3-upload-step-function-trigger/README.md
+++ aws-s3-upload-step-function-trigger/README.md
@@ -1,4 +1,4 @@
-# S3 upload trigger lambda function
+# Trigger a step function after file uploaded to an S3 bucket
 
 ### What is this?
 A serverless application that demonstrates how to trigger a step function after a file is uploaded into an S3 bucket.
--- aws-s3-upload-step-function-trigger/lib/trigger.py
+++ aws-s3-upload-step-function-trigger/lib/trigger.py
@@ -9,11 +9,7 @@ client = boto3.client('stepfunctions')
 def handler(event, context):
     print(event)
     for record in event['Records']:
-        trigger_workflow(record)
-
-
-def trigger_workflow(event_record):
-    response = client.start_execution(
-        stateMachineArn=my_state_machine_arn,
-        input=json.dumps(event_record['s3'])
-    )
+        response = client.start_execution(
+            stateMachineArn=my_state_machine_arn,
+            input=json.dumps(record['s3'])
+        )
